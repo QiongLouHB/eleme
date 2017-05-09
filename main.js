26314799d121d357e15206bbfe823253d109ee9c
QiongLouHB <983151921@qq.com>
2017-05-09 20:43:57 +0800

    new1

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 document.getElementById("box").onclick = function(){
 	alert('被点击咯');
+	alert('被点击咯2222222222');
+	alert('代码提交到暂存区');
+	alert('代码提交到暂存区2222');
+	
+	alert("ofdsjofdsjvcodszc");
+	alert("ofdsjofdsjvcodszc");
+	alert("ofdsjofdsjvcodszc");
+	alert("ofdsjofdsjvcodszc");
+	alert("ofdsjofdsjvcodszc");
+	alert("new1");
+	alert("new1");
+	alert("new1");
+	alert("new1");
+	alert("new1");
+	
+	
 }
